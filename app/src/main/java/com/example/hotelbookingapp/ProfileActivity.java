7aae50552dcec21ff02451731847875dbe8295b7
= <=>
2024-04-02 13:48:07 +0700

    layout

--- app/src/main/java/com/example/hotelbookingapp/ProfileActivity.java
+++ app/src/main/java/com/example/hotelbookingapp/ProfileActivity.java
@@ -1,7 +1,6 @@
 package com.example.hotelbookingapp;
 
 import android.os.Bundle;
-import android.widget.SearchView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CustomerMainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
-    SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_customer_main);
+        setContentView(R.layout.activity_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        searchView = findViewById(R.id.search_view);
-        searchView.setQueryHint("Tìm kiếm khách sạn");
-
-
     }
 }
