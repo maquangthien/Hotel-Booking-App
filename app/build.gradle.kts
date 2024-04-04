bfea3d1a8f6b2f7e07070a1e5eb886a82f0166be
phunglonghoang <phunglonghoang17081302@gmail.com>
2024-04-04 12:01:14 +0700

    bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
     implementation("org.mindrot:jbcrypt:0.4")
+    implementation("com.cloudinary:cloudinary-android:2.5.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
